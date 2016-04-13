298b8d36a9a80caf1d0a64e955815f6c74d7f249
The Cowans <Cowans1@thes-macbook-pro.local>
2016-04-13 06:55:08 +0100

    Initial Commit

---
+++ AngleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AngleTest
+//
+//  Created by The Cowans on 13/04/2016.
+//  Copyright © 2016 DCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AngleTest/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  AngleTest
+//
+
+//  Copyright (c) 2016 DCC. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
